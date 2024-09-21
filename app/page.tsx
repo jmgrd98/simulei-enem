c6b07b2b593275f6dd33d32f83bb832bd69cf43c
joaodantasdev <jmgrd98@gmail.com>
2024-09-21 04:08:12 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,31 +2,42 @@
 
 import { Button } from "@/components/ui/button";
 import axios from 'axios';
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [question, setQuestion] = useState<any>(null);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    generateQuestion();
-  }, [])
   const generateQuestion = async () => {
-    
+    setLoading(true);
     try {
       const response = await axios.get('https://api.enem.dev/v1/exams/2023/questions/1')
-      console.log(response)
+      setQuestion(response.data);
+      console.log(question)
     } catch (error) {
       console.error(error);
+    } finally {
+      setLoading(false);
     }
-
   }
 
   return (
     <>
       <main className="flex flex-col items-center justify-evenly p-24">
         <h1 className="text-3xl font-bold underline">Gerador de Questão Enem</h1>
-        <Button variant={'secondary'} onClick={generateQuestion}>Gerar Questão</Button>
 
+        <Button variant={'secondary'} onClick={generateQuestion} disabled={loading}>
+          {loading ? 'Carregando...' : 'Gerar Questão'}
+        </Button>
 
+        {question && (
+          <div className="mt-8 p-4 border rounded shadow">
+            <h2 className="text-xl font-semibold">Questão {question.index}</h2>
+            <p className="font-bold">Disciplina: {question.discipline}</p>
+            <p>{question.alternativesIntroduction}</p>
+            <p>{question.context}</p>
+          </div>
+        )}
 
       </main>
     </>
