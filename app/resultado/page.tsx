558a3e77c122d5ad5c41c6afff2638419b8b6155
joaodantasdev <jmgrd98@gmail.com>
2024-10-27 13:43:11 -0300

    feat(*) - non answered questions calculation

--- app/resultado/page.tsx
+++ app/resultado/page.tsx
@@ -8,7 +8,14 @@ import { useQuery } from '@tanstack/react-query';
 import axios from "axios";
 import Loader from "@/components/Loader/Loader";
 import { Pie, PieChart, Label } from "recharts";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 import { useSearchParams } from 'next/navigation';
 import { useExamTime } from "@/context/ExamTimeContext";
 import { Question } from '@prisma/client';
@@ -21,9 +28,8 @@ export default function ResultadoPage() {
   const { isSignedIn } = useUser();
   const searchParams = useSearchParams();
   const timeParam = searchParams.get('time');
-  
-  const totalQuestions = 180;
 
+  const totalQuestions = 180;
 
   const timeSpent = useMemo(() => {
     const totalExamTime = Number(timeParam) || selectedTime;
@@ -38,10 +44,30 @@ export default function ResultadoPage() {
     stopTimer();
   }, []);
 
+  // Calculate non-answered questions
+  const answeredQuestionsCount = selectedAnswers.length;
+  const unansweredQuestionsCount = totalQuestions - answeredQuestionsCount;
+
+  // Update chart data to include non-answered questions
   const chartData = [
     { label: "Corretas", value: score, fill: "#22C55E" },
-    { label: "Erradas", value: totalQuestions - score, fill: "#EF4444" }
+    { label: "Erradas", value: totalQuestions - score - unansweredQuestionsCount, fill: "#EF4444" },
+    { label: "Não respondidas", value: unansweredQuestionsCount, fill: "grey" }
   ];
+
+  const chartConfig = {
+    corretasVsErradas: {
+      label: "Corretas x Erradas",
+    },
+    corretas: {
+      label: "Corretas",
+      color: "hsl(var(--chart-1))",
+    },
+    erradas: {
+      label: "Erradas",
+      color: "hsl(var(--chart-2))",
+    }
+  } satisfies ChartConfig
 
   const fetchQuestions = async (): Promise<Question[]> => {
     const year = 2023;
@@ -75,96 +101,128 @@ export default function ResultadoPage() {
           {isSignedIn ? <div className="w-20 h-20"><UserButton /></div> : <Button variant={'secondary'} className="w-24 self-start font-semibold text-lg" size={'xl'} onClick={() => router.push('/sign-in')}>Login</Button>}
         </div>
 
-        <div className="flex flex-col gap-5 items-center">
-          <h1 className="text-xl font-semibold">Esse foi o seu resultado:</h1>
-          <div className="flex items-center justify-center gap-2">
-            <p className="text-lg text-green-600">Questões Corretas: {score}</p>
-            <p className="text-lg text-red-600">Questões Erradas: {totalQuestions - score}</p>
-          </div>
+        <Tabs defaultValue="questions" className="w-full max-w-3xl">
+          <TabsList className="flex justify-center space-x-2 p-1 rounded-md bg-transparent mb-5">
+            <TabsTrigger 
+              value="questions" 
+              className="text-[16px] px-4 py-2 rounded-md transition-colors duration-300 ease-in-out hover:bg-black/50 data-[state=active]:bg-zinc-600 data-[state=active]:text-white"
+            >
+              Questões
+            </TabsTrigger>
+            <TabsTrigger 
+              value="statistics" 
+              className="text-[16px] px-4 py-2 rounded-md transition-colors duration-300 ease-in-out hover:bg-black/50 data-[state=active]:bg-zinc-600 data-[state=active]:text-white"
+            >
+              Estatísticas
+            </TabsTrigger>
+          </TabsList>
 
-          <div className="flex items-center justify-center gap-2">
-            <p className="text-lg text-blue-600">
-              Teste feito em {hoursSpent} horas, {minutesSpent} minutos e {secondsSpent} segundos
-            </p>
-          </div>
-
-          <div className="flex items-center gap-2 flex-wrap pl-5">
-            {isLoading ? (
-              <div className="flex justify-center items-center h-[50vh] w-full m-auto">
-                <Loader />
+          <TabsContent value="questions">
+            <div className="flex flex-col gap-5 items-center">
+              <h1 className="text-xl font-semibold">Esse foi o seu resultado:</h1>
+              <div className="flex items-center justify-center gap-2">
+                <p className="text-lg text-green-600">Questões Corretas: {score}</p>
+                <p className="text-lg text-red-600">Questões Erradas: {totalQuestions - score - unansweredQuestionsCount}</p>
+                <p className="text-lg text-gray-600">Questões Não Respondidas: {unansweredQuestionsCount}</p>
               </div>
-            ) : isError ? (
-              <p>Erro ao carregar as questões. Tente novamente mais tarde.</p>
-            ) : (
-              questions!.map((question, index) => {
-                const selectedAnswer = selectedAnswers.find(answer => answer.index === index + 1)?.answer;
-                
-                let colorClass = 'bg-gray-400';
-                if (selectedAnswer) {
-                  colorClass = selectedAnswer === question.correctAlternative ? 'bg-green-500' : 'bg-red-500';
-                }
-                return (
-                  <div key={index} className={`w-10 h-10 flex items-center justify-center ${colorClass} text-white`}>
-                    {index + 1}
+
+              <div className="flex items-center justify-center gap-2">
+                <p className="text-lg text-blue-600">
+                  Teste feito em {hoursSpent} horas, {minutesSpent} minutos e {secondsSpent} segundos
+                </p>
+              </div>
+              <div className="flex items-center gap-2 flex-wrap pl-5">
+                {isLoading ? (
+                  <div className="flex justify-center items-center h-[50vh] w-full m-auto">
+                    <Loader />
                   </div>
-                );
-              })
-            )}
-          </div>
+                ) : isError ? (
+                  <p>Erro ao carregar as questões. Tente novamente mais tarde.</p>
+                ) : (
+                  questions!.map((question, index) => {
+                    const selectedAnswer = selectedAnswers.find(answer => answer.index === index + 1)?.answer;
 
-          <Card className="flex flex-col">
-            <CardHeader className="items-center pb-0">
-              <CardTitle>Respostas - Corretas vs Erradas</CardTitle>
-            </CardHeader>
-            <CardContent className="flex-1 pb-0">
-              <div className="mx-auto max-h-[250px]">
-                <PieChart width={250} height={250}>
-                  <Pie
-                    data={chartData}
-                    dataKey="value"
-                    nameKey="label"
-                    innerRadius={60}
-                    outerRadius={80}
-                    fill="#8884d8"
-                    strokeWidth={5}
-                  >
-                    <Label
-                      content={({ viewBox }) => {
-                        if (viewBox && "cx" in viewBox && "cy" in viewBox) {
-                          return (
-                            <text
-                              x={viewBox.cx}
-                              y={viewBox.cy}
-                              textAnchor="middle"
-                              dominantBaseline="middle"
-                            >
-                              <tspan
-                                x={viewBox.cx}
-                                y={viewBox.cy}
-                                className="fill-foreground text-3xl font-bold"
-                              >
-                                {totalQuestions}
-                              </tspan>
-                              <tspan
-                                x={viewBox.cx}
-                                y={(viewBox.cy || 0) + 24}
-                                className="fill-muted-foreground"
-                              >
-                                Total
-                              </tspan>
-                            </text>
-                          );
-                        }
-                      }}
-                    />
-                  </Pie>
-                </PieChart>
+                    let colorClass = 'bg-gray-400';
+                    if (selectedAnswer) {
+                      colorClass = selectedAnswer === question.correctAlternative ? 'bg-green-500' : 'bg-red-500';
+                    }
+                    return (
+                      <div key={index} className={`w-10 h-10 flex items-center justify-center ${colorClass} text-white`}>
+                        {index + 1}
+                      </div>
+                    );
+                  })
+                )}
               </div>
-            </CardContent>
-          </Card>
+            </div>
+          </TabsContent>
 
-          <Button variant={'secondary'} size={'xl'} onClick={handleBackToHome}>Voltar ao Início</Button>
-        </div>
+          <TabsContent value="statistics">
+            <div className="flex flex-col gap-5 items-center">
+              <Card className="flex flex-col">
+                <CardHeader className="items-center pb-0">
+                  <CardTitle>Respostas</CardTitle>
+                </CardHeader>
+                <CardContent className="flex-1 pb-0">
+                  <ChartContainer
+                    config={chartConfig}
+                    className="mx-auto aspect-square max-h-[250px]"
+                  >
+                    <div className="mx-auto max-h-[250px]">
+                      <PieChart width={250} height={250}>
+                        <ChartTooltip
+                          cursor={false}
+                          content={<ChartTooltipContent hideLabel />}
+                        />
+                        <Pie
+                          data={chartData}
+                          dataKey="value"
+                          nameKey="label"
+                          innerRadius={60}
+                          outerRadius={80}
+                          fill="#8884d8"
+                          strokeWidth={5}
+                        >
+                          <Label
+                            content={({ viewBox }) => {
+                              if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                                return (
+                                  <text
+                                    x={viewBox.cx}
+                                    y={viewBox.cy}
+                                    textAnchor="middle"
+                                    dominantBaseline="middle"
+                                  >
+                                    <tspan
+                                      x={viewBox.cx}
+                                      y={viewBox.cy}
+                                      className="fill-foreground text-3xl font-bold"
+                                    >
+                                      {totalQuestions}
+                                    </tspan>
+                                    <tspan
+                                      x={viewBox.cx}
+                                      y={(viewBox.cy || 0) + 24}
+                                      className="fill-muted-foreground"
+                                    >
+                                      Questões
+                                    </tspan>
+                                  </text>
+                                );
+                              }
+                            }}
+                          />
+                        </Pie>
+                      </PieChart>
+                    </div>
+                  </ChartContainer>
+                </CardContent>
+              </Card>
+            </div>
+          </TabsContent>
+        </Tabs>
+
+        <Button variant={'secondary'} size={'xl'} onClick={handleBackToHome} className="mt-5">Voltar ao Início</Button>
       </main>
     </>
   );
